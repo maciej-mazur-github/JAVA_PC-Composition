5cf972baf4ca40819b775c84e71f7af40e59d54b
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-04-04 17:36:08 +0200

    Add PC class

---
+++ src/PC.java
@@ -0,0 +1,22 @@
+import javax.management.monitor.Monitor;
+
+public class PC {
+    private Case theCase;
+    private Motherboard motherboard;
+    private Monitor monitor;
+
+    public PC(Case theCase, Motherboard motherboard, Monitor monitor) {
+        this.theCase = theCase;
+        this.motherboard = motherboard;
+        this.monitor = monitor;
+    }
+
+    public void powerUp() {
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo() {
+        monitor.drawPixelAt(1700, 20, "green");
+    }
+}
